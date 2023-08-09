32bc7655a0f5e210ddfb699185b084cfcad56eec
Martin Stofko <martinstofko114@gmail.com>
2023-08-09 13:36:30 -0400

    Initial Commit

---
+++ SwiftDataCourse/SwiftDataCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCourseApp.swift
+//  SwiftDataCourse
+//
+//  Created by Martin Stofko on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
